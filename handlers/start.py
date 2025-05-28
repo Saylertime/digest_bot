f807a76ff5f347ffa7527b03448255fbf11213a6
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2025-05-28 13:09:58 +0300

    fix for start

--- handlers/start.py
+++ handlers/start.py
@@ -8,5 +8,10 @@ router_start = Router()
 
 @router_start.message(CommandStart())
 async def command_start_handler(message):
-    await add_user(telegram_id=str(message.from_user.id))
+    if message.chat.type == "private":
+        telegram_id = str(message.from_user.id)
+    else:
+        telegram_id = str(message.chat.id)
+
+    await add_user(telegram_id=telegram_id)
     await message.answer(f"Йо! Дайджест приходит в 21:00 каждый день")
